8430e940728d00e1066c0d1890aaacdda8917e45
Kendrick80 <agyeikendrick@gmaol.com>
2023-05-16 05:35:10 +0000

    form

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 
 const App=()=> {
+  const [formData, setFormData]=React.useState({
+    name:"Bing",
+    value:" "
+  })
+  function toggleChange(event){
+    const {name,value}=event.target
+    setFormData(previousData=>({
+      ...previousData,[name]:value
+    }))
+  }
   return (
-<div>
-  
+<div className="form">
+<div className='Box'>
+
+</div>
+  <form>
+    <input
+      type='text'
+      className='txt'
+      name={formData.name}
+      value={formData.value}
+      onChange={toggleChange}
+    />
+  </form>
 </div>
   );
 };
